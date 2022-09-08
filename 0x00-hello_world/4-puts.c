1f193a0b0ed3aa97cb4137e30da26c8069e462f5
nati0021 <nathanbiru21@gmai.com>
2022-09-09 01:55:04 +0300

    not print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *
- *int main()  
- *
- *return 0;
+ *main -main block
+ *Return: 0
  */
- {
-  puts("programming is like building a miltilingual puzzle.\n");
-  getchar();
+int main(void)
+{
+  puts("\"programming is like building a multilingual puzzle");
+  return (0);
 }
