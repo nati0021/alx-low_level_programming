ac19e54786b1057c164a173ceb0a80b65087137c
nati0021 <nathanbiru21@gmai.com>
2022-09-09 02:49:53 +0300

    jmvk

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+
 /**
  * main - Entry point
- * Return:1
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, ch, sizeof(ch));
-return (1);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
