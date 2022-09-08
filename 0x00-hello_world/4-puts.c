db5d62fb2b710111af03526e21c6a56d1ccbeebc
nati0021 <nathanbiru21@gmai.com>
2022-09-09 02:30:15 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
+
 #include <stdio.h>
+
 /**
-*main -main block
-*Return: 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle.\n");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
