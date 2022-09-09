5605ca59d7c536d2031577496c85e46de244003d
nati0021 <nathanbiru21@gmai.com>
2022-09-09 21:56:20 +0300

    both

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - main block
+ *
+ *
+ *Return: 0
+ */
+int main(void)
+{
+  char c = 'a';
+
+  while (c <= 'Z')
+    {
+      putchar(c);
+      c++;
+    }
+  c <= 'Z'
+    {
+      putchar(c);
+      c++;
+    }
+  
+  putchar('\n');
+  return (0);
+}
+
+	 
+      
