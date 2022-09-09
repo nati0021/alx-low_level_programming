960fb39fd7d6c5be0f7e9692549f3627886d9541
nati0021 <nathanbiru21@gmai.com>
2022-09-09 15:03:46 +0300

    project0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+  int n;
+/**
+ *program to display a number
+ *
+ */
+  if (n < 0){
+    printf("is negative.\n");
+  }
+  if (n > 0){
+    printf("is positive.\n");
+  }
+  if (n == 0){
+    printf("is zero./n");
+  }
+    return 0;
+}
