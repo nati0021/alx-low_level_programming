0c4b9fc1361dcd2a1c5b04a958f879848dc062b7
nati0021 <nathanbiru21@gmai.com>
2022-09-09 23:06:15 +0300

    number

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,19 +3,17 @@
 #include <stdlib.h>
 /*
  *main - main block
- *
+ *Discription: prints all single digit numbers of base 10
  *Return: 0
  */
 int main(void)
 {
 char c = '0';
-
 while (c <= '9')
 {
 putchar(c);
 c++;
 }
-
 putchar('\n');
 return (0);
 }  
