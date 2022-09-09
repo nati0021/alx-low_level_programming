d86621ae3410d43a2b27da151474548aab77dad3
nati0021 <nathanbiru21@gmai.com>
2022-09-09 23:23:25 +0300

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
