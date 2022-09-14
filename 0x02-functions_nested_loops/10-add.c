c1b86d96ad247f4b8444c0e9916d83b16dbe68d2
nati0021 <nathanbiru21@gmai.com>
2022-09-14 23:55:54 +0300

    the rest

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - check the code
+ * @x: an input character
+ * @y: an input character
+ * gives sum of two numbers
+ * Return: sum
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+
+	return (sum);
+}
