c3ea1d55914853d65ab63b896ab04c3aaf5eddf7
nati0021 <nathanbiru21@gmail.com>
2022-10-01 13:59:41 +0300

    commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,11 +12,10 @@ int main(int argc, char const *argv[])
 (void)argc;
 
 if (argc >= 3)
-    {
-      printf("Error\n");
-      return (1);
-    }
-else
-    printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+{
+printf("Error\n");
+return (1);
+}
+printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
