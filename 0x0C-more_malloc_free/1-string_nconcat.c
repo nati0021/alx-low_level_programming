5e9ef8aa30f33d03d051a4658397efdcb755d034
nati0021 <nathanbiru21@gmail.com>
2022-10-07 01:29:50 +0300

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,13 +14,6 @@ for (; s[size] != '\0'; size++)
 return (size);
 }
 
-/**
- * *string_nconcat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * @n: first bytes of s2 to be used
- * Return: pointer or NULL
- */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
